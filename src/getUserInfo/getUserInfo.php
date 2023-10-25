059c8db43fa82fea08bfe94479e38a69a043eded
Christoph <christoph@rustige.me>
2023-10-25 09:55:14 +0200

    Initial Class Setup

---
+++ src/getUserInfo/getUserInfo.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: getUserInfo
+Plugin URI: https://github.com/miit0o/getUserInfo
+Description: A simple plugin to enable showing the current users IP address and hostname. You can use the shortcode [show_ip] to view the users IP address or [show_hostname] to show the users hostname on any page.
+Version: 1.0
+Requires at least: 4.8
+Tested up to: 5.5
+Requires PHP: 5.6
+Author: miit0o
+Author URI: https://rustige.me
+License: MIT
+
+Copyright (c) 2023 Christoph Rustige. All rights reserved.
+*/
+
+function getUserInfo() {
+	
+?>
